b1d3bce58520d687ea16135a15ccec915dceb636
forteller11 <charly.yan.miller@gmail.com>
2020-09-14 18:28:10 -0400

    created a shader class which compiles vert/frag text into functioning shader

--- independent-study-fall-2020/c_sharp/Shader.cs
+++ independent-study-fall-2020/c_sharp/Shader.cs
@@ -1,26 +1,54 @@
 ﻿using System;
 using System.IO;
+using OpenTK.Graphics.OpenGL4;
 
 namespace Indpendent_Study_Fall_2020
 {
     public class Shader
     {
-        int Handle;
+        public int Handle { get; private set; }
 
-        public Shader(string vertexFileName, string fragmentFileName)
+
+        public Shader(string vertexFileName, string fragmentFileName) //TODO capsulate stage of graphics pipeline into class (frag, vert, geo...)
         {
             string vertexFile;
             string fragmentFile;
             
-            using (var streamReader = new StreamReader(SerializationManager.ShaderPath + "\\" + vertexFileName))
+            using (var streamReader = new StreamReader(SerializationManager.ShaderPath + "\\" + vertexFileName)) //read text from file
                 vertexFile = streamReader.ReadToEnd();
             using (var streamReader = new StreamReader(SerializationManager.ShaderPath + "\\" + fragmentFileName))
                 fragmentFile = streamReader.ReadToEnd();
+
+            int vertexHandle = GL.CreateShader(ShaderType.VertexShader); //create handles
+            int fragmentHandle = GL.CreateShader(ShaderType.VertexShader);
             
-            Console.WriteLine(vertexFile);
-            Console.WriteLine(fragmentFile);
+            GL.ShaderSource(vertexHandle, vertexFile); //bind handle to text
+            GL.ShaderSource(fragmentHandle, fragmentFile);
             
+            GL.CompileShader(vertexHandle);
+            GL.CompileShader(fragmentHandle);
+
+            Handle = GL.CreateProgram();
+            GL.AttachShader(Handle, vertexHandle);
+            GL.AttachShader(Handle, fragmentHandle);
+            GL.LinkProgram(Handle);
+            
+            GL.DetachShader(Handle, vertexHandle);
+            GL.DetachShader(Handle, fragmentHandle);
+            GL.DeleteShader(vertexHandle);
+            GL.DeleteShader(fragmentHandle);
 
         }
+
+        public void Use()
+        {
+            GL.UseProgram(Handle);
+        }
+        
+        ~Shader()
+        {
+            GL.DeleteProgram(Handle);
+        }
+        //TODO implement iDisposable to free up memory?? what is wrong with using destructor? is it that an exception will be thrown if openGL context is not active? put program.delete in window.unload() then
     }
 }
