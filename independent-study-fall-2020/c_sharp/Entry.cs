2314a3371bbb710269e691f4b040b78371539d5b
forteller11 <charly.yan.miller@gmail.com>
2020-09-17 03:49:16 -0400

    loading image via sixlabors.ImageSharp

--- independent-study-fall-2020/c_sharp/Entry.cs
+++ independent-study-fall-2020/c_sharp/Entry.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using OpenTK.Graphics.OpenGL4;
 using OpenTK.Input;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
 
 // alot of code is taken from the openTK tutorials here: https://opentk.net/learn
 
@@ -9,10 +13,36 @@ namespace Indpendent_Study_Fall_2020
 {
     static class Entry
     {
+        
+        private static byte[] texturePixels;
         private static void Main(string[] _)
         {
 
-      var glWindow = TKWindow.CreateAndRun();
+//             var glWindow = TKWindow.CreateAndRun();
+            
+             
+             //Load the image
+             string path = SerializationManager.AssetPath + "unwrap_helper.jpg";
+             var image =  Image.Load<Rgba32>(path);
+
+//ImageSharp loads from the top-left pixel, whereas OpenGL loads from the bottom-left, causing the texture to be flipped vertically.
+//This will correct that, making the texture display properly.
+             image.Mutate(x => x.Flip(FlipMode.Vertical));
+
+//Get an array of the pixels, in ImageSharp's internal format.
+             image.TryGetSinglePixelSpan(out var tempPixels);
+
+//Convert ImageSharp's format into a byte array, so we can use it with OpenGL.
+             List<byte> pixels = new List<byte>();
+
+             foreach (Rgba32 p in tempPixels)
+             {
+//                 Console.Write(p.R + "|");
+                 pixels.Add(p.R);
+                 pixels.Add(p.G);
+                 pixels.Add(p.B);
+                 pixels.Add(p.A);
+             }
         }
     }
 }
--- independent-study-fall-2020/c_sharp/SerializationManager.cs
+++ independent-study-fall-2020/c_sharp/SerializationManager.cs
@@ -3,6 +3,7 @@
     public static class SerializationManager
     {
         public static string ProjectPath = @"D:\MOVE\Homework\Concordia_Year_03\Independent_Study\Repos\independent-study-fall-2020\independent-study-fall-2020";
+        public static string AssetPath = @"D:\MOVE\Homework\Concordia_Year_03\Independent_Study\Repos\independent-study-fall-2020\assets\";
         public static string ShaderPath = ProjectPath + "\\Shaders";
     }
 }
