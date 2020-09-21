b184a4db3baaebfb76ea1af034996e1aa0975f1c
forteller11 <charly.yan.miller@gmail.com>
2020-09-20 23:40:33 -0400

    continuing refactoring

--- independent-study-fall-2020/c_sharp/MaterialRelated/Material.cs
+++ independent-study-fall-2020/c_sharp/MaterialRelated/Material.cs
@@ -10,12 +10,15 @@ namespace Indpendent_Study_Fall_2020
         private ShaderProgram _shader;
         private Dictionary<string, int> Attrib;
         private List<Uniform> _uniforms;
+        private int _vboHandle;
+        private int _vaoHandle;
         private float[] _buffer;
 
         //combine multiple vbos into single vbo, then automagically vao it up
         //todo, join multiple buffers into single vbo each with names, create vao
         void JoinBuffersAndCreateAttributes(params AttributeBuffer [] attributeBuffers)
         {
+            int totalStrideLength = 0;
             for (int i = 0; i < attributeBuffers.Length; i++)
             {
                 if (attributeBuffers[i].VerticesCount != attributeBuffers[0].VerticesCount)
@@ -23,9 +26,10 @@ namespace Indpendent_Study_Fall_2020
                     throw new Exception($"The attribute \"{attributeBuffers[i].AttributeName}\"" +
                                         $" doesn't have the same number of vertices as attribute: \"{attributeBuffers[i].AttributeName}!\"");
                 }
+
+                totalStrideLength += attributeBuffers[i].Stride;
             } //error checking
             
-            //todo do joining, add names and handle to dictionary after.... create vao
             for (int vertIndex = 0; vertIndex < attributeBuffers.Length; vertIndex++)
             {
                 for (int strideIndex = 0; strideIndex < attributeBuffers[vertIndex].Stride; strideIndex++)
@@ -34,9 +38,44 @@ namespace Indpendent_Study_Fall_2020
                     _buffer[startOfStride + strideIndex] = attributeBuffers[vertIndex].Buffer[strideIndex];
                 }
             }
+
+            //todo get dictionary wokring
+//            for (int i = 0; i < UPPER; i++)
+//            {
+//                //does calling get attriblocation() work b4 it's actually bound in shader?
+//            }
+
+            _shader.Use();
             
-            //upload vbo
-            //do vao work
+            _vboHandle = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vboHandle);
+            GL.BufferData(BufferTarget.ArrayBuffer, _buffer.Length * sizeof(float), _buffer, BufferUsageHint.StaticDraw);
+
+            _vaoHandle = GL.GenVertexArray();
+            GL.BindVertexArray(_vaoHandle);
+            int offset = 0;
+            for (int i = 0; i < attributeBuffers.Length; i++)
+            {
+                GL.VertexAttribPointer(
+                    GL.GetAttribLocation(_shader.Handle, attributeBuffers[i].AttributeName),
+                    attributeBuffers[i].Stride,
+                    VertexAttribPointerType.Float,
+                    false,
+                    totalStrideLength * sizeof(float),
+                    offset
+                );
+                GL.EnableVertexAttribArray(_shader.GetAttribLocation(attributeBuffers[i].AttributeName));
+                   
+               offset += attributeBuffers[i].Stride * sizeof(float);
+            }
+
+        }
+
+        //todo, create way to use vao, and then draw using mat
+        void Draw()
+        {
+            GL.BindVertexArray(_vaoHandle);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
         }
 
         int UseAttrib(string attributeName)
---
+++ independent-study-fall-2020/c_sharp/MaterialRelated/VertexArrayObject.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenTK.Graphics.OpenGL4;
+
+namespace Indpendent_Study_Fall_2020
+{
+    
+    public struct VertexArrayObject
+    {
+        private float[] _buffer;
+        public int VerticesCount { get; private set; }
+        public int StrideLength { get; private set; }
+        public int VBOHandle { get; private set; }
+        public int VAOHandle { get; private set; }
+        public Dictionary<string, int> AttributeIndex;
+
+        void JoinBuffersAndCreateAttributes(ShaderProgram program, params AttributeBuffer [] attributeBuffers)
+        {
+            int StrideLength = 0;
+            int VerticesCount = attributeBuffers[0].VerticesCount;
+            AttributeIndex = new Dictionary<string, int>(attributeBuffers.Length);
+            
+            for (int i = 0; i < attributeBuffers.Length; i++)
+            {
+                if (attributeBuffers[i].VerticesCount != attributeBuffers[0].VerticesCount) //error checking
+                {
+                    throw new Exception($"The attribute \"{attributeBuffers[i].AttributeName}\"" +
+                                        $" doesn't have the same number of vertices as attribute: \"{attributeBuffers[i].AttributeName}!\"");
+                }
+//                AttributeIndex.Add(GL.GetAttribLocation(program.Handle, attributeBuffers[i].AttributeName), i);
+                StrideLength += attributeBuffers[i].Stride;
+            }
+            
+            for (int vertIndex = 0; vertIndex < attributeBuffers.Length; vertIndex++)
+            {
+                for (int strideIndex = 0; strideIndex < attributeBuffers[vertIndex].Stride; strideIndex++)
+                {
+                    int startOfStride = vertIndex * attributeBuffers[vertIndex].Stride;
+                    _buffer[startOfStride + strideIndex] = attributeBuffers[vertIndex].Buffer[strideIndex];
+                }
+            }
+
+            //to
+            
+            VBOHandle = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VBOHandle);
+            GL.BufferData(BufferTarget.ArrayBuffer, _buffer.Length * sizeof(float), _buffer, BufferUsageHint.StaticDraw);
+
+            VAOHandle = GL.GenVertexArray();
+            GL.BindVertexArray(VAOHandle);
+            int offset = 0;
+            for (int i = 0; i < attributeBuffers.Length; i++)
+            {
+                GL.VertexAttribPointer(
+                    GL.GetAttribLocation(program.Handle, attributeBuffers[i].AttributeName),
+                    attributeBuffers[i].Stride,
+                    VertexAttribPointerType.Float,
+                    false,
+                    StrideLength * sizeof(float),
+                    offset
+                );
+                GL.EnableVertexAttribArray(program.GetAttribLocation(attributeBuffers[i].AttributeName));
+                   
+               offset += attributeBuffers[i].Stride * sizeof(float);
+            }
+
+        }
+    }
+}
