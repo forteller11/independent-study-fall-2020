71587c18cd883966e2949281327749863e308877
forteller11 <charly.yan.miller@gmail.com>
2020-10-15 14:58:59 -0400

    cleaning up unused classes/interfaces

--- independent-study-fall-2020/c_sharp/MaterialRelated/Uniform.cs
+++
@@ -1,9 +0,0 @@
-﻿﻿namespace Indpendent_Study_Fall_2020.MaterialRelated
-{
-    public interface Uniform
-    {
-        string ShaderName { get; set; }
-        void UploadToShader();
-        void Use();
-    }
-}
